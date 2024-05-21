d36769731cbf431e1a26d6ba16c8deac44046767
Stephen Beer <atlassian@corner.plus.com>
2024-05-22 00:15:57 +0100

    Configured firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyC1TtDErzx62M9mt4t34Bh_fauiv9UT3Ks",
+    authDomain: "friendlychat-558a5.firebaseapp.com",
+    projectId: "friendlychat-558a5",
+    storageBucket: "friendlychat-558a5.appspot.com",
+    messagingSenderId: "8167240706",
+    appId: "1:8167240706:web:600c99167e1c450688231b",
+    measurementId: "G-SHKFFXFN3K"
   },
 };
